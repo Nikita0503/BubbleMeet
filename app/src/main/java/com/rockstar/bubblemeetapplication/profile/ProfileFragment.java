8a844ee8b9612a1d7ec3e944ea7872a70fb28f58
nikita0503 <nikita0503ua@gmail.com>
2019-09-02 23:30:16 +0300

    profile markup(2/4)

--- app/src/main/java/com/rockstar/bubblemeetapplication/profile/ProfileFragment.java
+++ app/src/main/java/com/rockstar/bubblemeetapplication/profile/ProfileFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -19,8 +22,11 @@ import com.rockstar.bubblemeetapplication.R;
 public class ProfileFragment extends Fragment implements BaseContract.BaseView {
 
     String mName;
-
     TextView mTextViewName;
+    ImageView mImageViewLike;
+    ImageView mImageViewBoom;
+    ImageView mImageViewAvatar;
+    ImageView mImageViewBoomWhiteCircle;
     RecyclerView mRecyclerViewUsers;
 
     public void setName(String name){
@@ -36,17 +42,36 @@ public class ProfileFragment extends Fragment implements BaseContract.BaseView {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         mRecyclerViewUsers = (RecyclerView) view.findViewById(com.rockstar.bubblemeetapplication.R.id.recyclerViewUsers);
-        LinearLayoutManager layoutManager
-                = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        mRecyclerViewUsers.setLayoutManager(layoutManager);
-        mRecyclerViewUsers.setAdapter(new ProfileCustomAdapter());
         mTextViewName = (TextView) view.findViewById(R.id.textViewName);
-        mTextViewName.setText(mName);
+        mImageViewLike = (ImageView) view.findViewById(R.id.imageViewLike);
+        mImageViewBoom = (ImageView) view.findViewById(R.id.imageViewBoom);
+        mImageViewAvatar = (ImageView) view.findViewById(R.id.imageViewAvatar);
+        mImageViewBoomWhiteCircle = (ImageView) view.findViewById(R.id.imageViewBoomWhiteCircle);
         initViews();
     }
 
     @Override
     public void initViews() {
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerViewUsers.setLayoutManager(layoutManager);
+        mRecyclerViewUsers.setAdapter(new ProfileCustomAdapter());
+        mTextViewName.setText(mName);
+        mImageViewLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mImageViewLike.setVisibility(View.INVISIBLE);
+                mImageViewBoom.setVisibility(View.VISIBLE);
+                Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.scale_like);
+                mImageViewBoom.startAnimation(animation);
 
+                //Animation animation2 = AnimationUtils.loadAnimation(getContext(), R.anim.for_swipe_down);
+                //mImageViewAvatar.startAnimation(animation2);
+
+                //Animation animation3 = AnimationUtils.loadAnimation(getContext(), R.anim.white_circle_boom_scale);
+                //mImageViewBoomWhiteCircle.setVisibility(View.VISIBLE);
+                //mImageViewBoomWhiteCircle.startAnimation(animation3);
+            }
+        });
     }
 }
